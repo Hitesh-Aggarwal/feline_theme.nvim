59e96ed65c77b7a8b8cff8acb2ea1ebf6154fc5f
Hitesh Aggarwal <hitesh33231@gmail.com>
2022-10-18 21:07:40 +0530

    upgrade to 0.8

--- README.md
+++ README.md
@@ -4,12 +4,17 @@ This is a feline configuration based on [one_monokai](https://github.com/cpea250
 
 ## Requirements:
 
-- Neovim >= 0.7
+- Neovim >= 0.8
 - [feline.nvim](https://github.com/feline-nvim/feline.nvim)
+- [nvim-navic](https://github.com/SmiteshP/nvim-navic)
 
 ## Screenshots:
 
+#### Statusline
 <img src="./screenshot.png">
+
+#### Winbar
+<img src="./winbar.png">
 
 ## Installation:
 
@@ -28,5 +33,3 @@ Plug 'Hitesh-Aggarwal/feline_one_monokai.nvim'
 ### Contribute
 
 **Please consider raising a pull request for improvements.**
-
-Since I am not a lua guru (I never learnt any lua, all i know is just because of neovim), your contribution means a lot. It would also help the community in general.
--- plugin/feline_one_monokai.lua
+++ plugin/feline_one_monokai.lua
@@ -1,207 +1,150 @@
-local line_ok, feline = pcall(require, "feline")
-if not line_ok then
+local feline_ok, feline = pcall(require, "feline")
+if not feline_ok then
   return
 end
 
-local one_monokai = {
-  fg = "#abb2bf",
-  bg = "#1e2024",
-  green = "#98c379",
-  yellow = "#e5c07b",
-  purple = "#c678dd",
+local tokyonight = {
+  fg = "#c8d3f5",
+  bg = "#1e2030",
+  bg_dark = "#222436",
+  green = "#c3e88d",
+  yellow = "#ffc777",
+  purple = "#fca7ea",
   orange = "#d19a66",
-  peanut = "#f6d5a4",
-  red = "#e06c75",
-  aqua = "#61afef",
-  darkblue = "#282c34",
-  dark_red = "#f75f5f",
+  fg_dark = "#a9b1d6",
+  red = "#ff757f",
+  blue = "#82aaff",
+  dark_red = "#c53b53",
 }
 
-local vi_mode_colors = {
-  NORMAL = "green",
-  OP = "green",
-  INSERT = "yellow",
-  VISUAL = "purple",
-  LINES = "orange",
-  BLOCK = "dark_red",
-  REPLACE = "red",
-  COMMAND = "aqua",
-}
-
-local c = {
-  vim_mode = {
-    provider = {
-      name = "vi_mode",
-      opts = {
-        show_mode_name = true,
-        -- padding = "center",
-      },
-    },
-    hl = function()
-      return {
-        fg = require("feline.providers.vi_mode").get_mode_color(),
-        bg = "darkblue",
-        style = "bold",
-        name = "NeovimModeHLColor",
-      }
-    end,
-    left_sep = "block",
-    right_sep = "block",
-  },
-  gitBranch = {
-    provider = "git_branch",
+local provide = function(provider, fg_color, bg_color, style, left_sep, right_sep, enabled)
+  provider = provider or " "
+  fg_color = fg_color or "fg"
+  bg_color = bg_color or "bg"
+  style = style or "NONE"
+  left_sep = left_sep or ""
+  right_sep = right_sep or ""
+  enabled = enabled or true
+  return {
+    provider = provider,
     hl = {
-      fg = "peanut",
-      bg = "darkblue",
-      style = "bold",
+      fg = fg_color,
+      bg = bg_color,
+      style = style,
     },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  gitDiffAdded = {
-    provider = "git_diff_added",
-    hl = {
-      fg = "green",
-      bg = "darkblue",
-    },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  gitDiffRemoved = {
-    provider = "git_diff_removed",
-    hl = {
-      fg = "red",
-      bg = "darkblue",
-    },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  gitDiffChanged = {
-    provider = "git_diff_changed",
-    hl = {
-      fg = "fg",
-      bg = "darkblue",
-    },
-    left_sep = "block",
-    right_sep = "right_filled",
-  },
-  separator = {
-    provider = "",
-  },
-  diagnostic_errors = {
-    provider = "diagnostic_errors",
-    hl = {
-      fg = "red",
-    },
-  },
-  diagnostic_warnings = {
-    provider = "diagnostic_warnings",
-    hl = {
-      fg = "yellow",
-    },
-  },
-  diagnostic_hints = {
-    provider = "diagnostic_hints",
-    hl = {
-      fg = "aqua",
-    },
-  },
-  diagnostic_info = {
-    provider = "diagnostic_info",
-  },
-  lsp_client_names = {
-    provider = "lsp_client_names",
-    hl = {
-      fg = "purple",
-      bg = "darkblue",
-      style = "bold",
-    },
-    left_sep = "left_filled",
-    right_sep = "block",
-  },
-  file_type = {
-    provider = {
-      name = "file_type",
-      opts = {
-        filetype_icon = true,
-        case = "titlecase",
-      },
-    },
-    hl = {
-      fg = "red",
-      bg = "darkblue",
-      style = "bold",
-    },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  position = {
-    provider = "position",
-    hl = {
-      fg = "green",
-      bg = "darkblue",
-      style = "bold",
-    },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  line_percentage = {
-    provider = "line_percentage",
-    hl = {
-      fg = "aqua",
-      bg = "darkblue",
-      style = "bold",
-    },
-    left_sep = "block",
-    right_sep = "block",
-  },
-  scroll_bar = {
-    provider = "scroll_bar",
-    hl = {
-      fg = "yellow",
-      style = "bold",
-    },
-  },
-}
+    left_sep = left_sep,
+    right_sep = right_sep,
+    enabled = enabled,
+  }
+end
 
 local left = {
-  c.vim_mode,
-  c.gitBranch,
-  c.gitDiffAdded,
-  c.gitDiffRemoved,
-  c.gitDiffChanged,
-  c.separator,
-}
-
-local middle = {
-  c.diagnostic_errors,
-  c.diagnostic_warnings,
-  c.diagnostic_info,
-  c.diagnostic_hints,
+  provide(" ", "blue", "blue"),
+  provide("git_branch", "purple", "bg", "bold", "block", "block"),
+  provide(
+    { name = "file_type", opts = { filetype_icon = true, case = "titlecase" } },
+    "fg",
+    "bg",
+    "NONE",
+    "block",
+    "block"
+  ),
+  provide " ",
+  provide("git_diff_added", "green"),
+  provide("git_diff_removed", "red"),
+  provide("git_diff_changed", "fg"),
+  provide(" ", "fg", "bg", "NONE", "block", "block"),
+  provide("diagnostic_errors", "dark_red"),
+  provide("diagnostic_warnings", "yellow"),
+  provide("diagnostic_hints", "blue"),
+  provide "diagnostic_info",
 }
 
 local right = {
-  c.lsp_client_names,
-  c.file_type,
-  c.position,
-  c.line_percentage,
-  c.scroll_bar,
+  provide("lsp_client_names", "fg_dark", "bg", "NONE", "block", "block"),
+  provide("position", "green", "bg", "NONE", "block", "block"),
+  provide("line_percentage", "orange", "bg", "NONE", "block", "block"),
+  provide(" ", "blue", "blue"),
+}
+
+local left_inactive = {
+  provide(" ", "blue", "blue"),
+  provide(
+    { name = "file_type", opts = { filetype_icon = true, case = "titlecase" } },
+    "fg",
+    "bg",
+    "NONE",
+    "block",
+    "block"
+  ),
+}
+
+local right_inactive = {
+  provide("position", "green", "bg", "NONE", "block", "block"),
+  provide("line_percentage", "orange", "bg", "NONE", "block", "block"),
+  provide(" ", "blue", "blue"),
 }
 
 local components = {
   active = {
     left,
-    middle,
     right,
   },
   inactive = {
-    left,
-    right,
+    left_inactive,
+    right_inactive,
   },
 }
 
-feline.setup({
+local components_winbar = {
+  active = {
+    {
+      provide(
+        { name = "file_info", opts = { type = "relative" } },
+        "blue",
+        "bg_dark",
+        "NONE",
+        "",
+        { str = "", hl = { fg = "bg_dark", bg = "bg_dark" } }
+      ),
+      provide(
+        " ",
+        "fg",
+        "bg_dark",
+        "NONE",
+        "",
+        { str = "slant_left_thin", hl = { bg = "bg_dark" } },
+        require("nvim-navic").is_available
+      ),
+      provide(
+        require("nvim-navic").get_location,
+        "fg",
+        "bg_dark",
+        "NONE",
+        "block",
+        { str = "", hl = { fg = "bg_dark", bg = "bg_dark" } },
+        require("nvim-navic").is_available
+      ),
+    },
+  },
+  inactive = {
+    {
+      provide(
+        { name = "file_info", opts = { type = "relative" } },
+        "blue",
+        "bg_dark",
+        "NONE",
+        "",
+        { str = "", hl = { fg = "bg_dark", bg = "bg_dark" } }
+      ),
+    },
+  },
+}
+
+feline.setup {
   components = components,
-  theme = one_monokai,
-  vi_mode_colors = vi_mode_colors,
-})
+  theme = tokyonight,
+}
+
+feline.winbar.setup { components = components_winbar }
+
