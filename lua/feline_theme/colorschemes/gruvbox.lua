e419f46ef510a3752b3c9c0062ca68c8879b7e27
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-01-12 13:39:44 +0530

    added gruvbox colorscheme

---
+++ lua/feline_theme/colorschemes/gruvbox.lua
@@ -0,0 +1,14 @@
+return {
+  fg = "#a89984",
+  bg = "#282828",
+  bg_dark = "#1d2021",
+  green = "#98971a",
+  yellow = "#d79921",
+  purple = "#b16286",
+  orange = "#d65d0e",
+  fg_dark = "#fbf1c7",
+  red = "#cc241d",
+  blue = "#458588",
+  dark_red = "#9d0006",
+}
+
