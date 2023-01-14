c064c9b8e3116200f16498aa85095aa77a17e19e
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-01-14 17:07:33 +0530

    add gruvbox_material_dark themes

---
+++ lua/feline_theme/colorschemes/gruvbox_material_dark_medium.lua
@@ -0,0 +1,13 @@
+return {
+  fg = "#d4be98",
+  bg = "#1b1b1b",
+  bg_dark = "#282828",
+  green = "#a9b665",
+  yellow = "#d8a657",
+  purple = "#d3869b",
+  orange = "#e78a4e",
+  fg_dark = "#ddc7a1",
+  red = "#ea6962",
+  blue = "#7daea3",
+  dark_red = "#c14a4a",
+}
