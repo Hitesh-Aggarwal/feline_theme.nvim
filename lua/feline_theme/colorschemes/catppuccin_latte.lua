0e83b8ddc278b814b622b7d58abd9119ff03acf2
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-01-26 20:14:29 +0530

    add catppuccin colorschemes

---
+++ lua/feline_theme/colorschemes/catppuccin_latte.lua
@@ -0,0 +1,15 @@
+return {
+  fg = "#4C4F69",
+  bg = "#d2d4d8",
+  bg_dark = "#EFF1F5",
+  green = "#40A02B",
+  yellow = "#df8e1d",
+  purple = "#ea76cb",
+  orange = "#FE640B",
+  fg_dark = "#dc8a78",
+  red = "#E64553",
+  blue = "#1e66f5",
+  dark_red = "#D20F39",
+}
+
+
---
+++ lua/feline_theme/colorschemes/catppuccin_macchiato.lua
@@ -0,0 +1,14 @@
+return {
+  fg = "#CAD3F5",
+  bg = "#303446",
+  bg_dark = "#24273A",
+  green = "#A6DA95",
+  yellow = "#EED49F",
+  purple = "#F5BDE6",
+  orange = "#F5A97F",
+  fg_dark = "#F4DBD6",
+  red = "#EE99A0",
+  blue = "#8AADF4",
+  dark_red = "#ED8796",
+}
+
