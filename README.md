4651747201ece2d42333f3a6b0c3c1f496e56e78
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-01-27 10:45:31 +0530

    add customization and update readme.

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ This is a feline opinionated feline config designed to be used as a plugin. It a
 ## Installation:
 
 ### Note: Make sure that you always load your colorscheme first.
+#### Anoter Note: You can't use only winbar. That will not work.
 
 ### Packer:
 
@@ -64,6 +65,39 @@ return {
     require("feline_theme").setup_winbar() -- for winbar
   end
 }
+```
+
+### Configuration:
+
+```
+local format = function(arg)
+  return "#" .. string.format("%06x", arg)
+end
+
+local giv_foreground = function(group)
+  return format(get_hl(group, true).foreground)
+end
+
+local giv_background = function(group)
+  return format(get_hl(group, true).background)
+end
+
+require("feline_theme").setup({
+  fg = giv_foreground("Normal"),
+  bg = giv_background("CursorLine"),
+  bg_dark = giv_background("Normal"),
+  green = giv_foreground("DiffAdded"),
+  yellow = giv_foreground("WarningMsg"),
+  purple = giv_foreground("Special"),
+  orange = giv_foreground("Number"),
+  red = giv_foreground("Error"),
+  blue = giv_foreground("Title"),
+  -- these are the defaults. You can change any color.
+  -- Do add or remove any other colors. Just change the values
+  -- of these predefined colors.
+  -- For example:
+  -- red = "#ff0000"
+})
 ```
 
 ## Contribute
--- lua/feline_theme/init.lua
+++ lua/feline_theme/init.lua
@@ -1,41 +1,41 @@
 local feline_ok, feline = pcall(require, "feline")
 if not feline_ok then
-	return
+  return
 end
 local components = {
-	active = {
-		require("feline_theme.statusline").left,
-		require("feline_theme.statusline").right,
-	},
-	inactive = {
-		require("feline_theme.statusline").left_inactive,
-		require("feline_theme.statusline").right_inactive,
-	},
+  active = {
+    require("feline_theme.statusline").left,
+    require("feline_theme.statusline").right,
+  },
+  inactive = {
+    require("feline_theme.statusline").left_inactive,
+    require("feline_theme.statusline").right_inactive,
+  },
 }
 
 local components_winbar = {
-	active = {
-		require("feline_theme.winbar").active,
-	},
-	inactive = {
-		require("feline_theme.winbar").inactive,
-	},
+  active = {
+    require("feline_theme.winbar").active,
+  },
+  inactive = {
+    require("feline_theme.winbar").inactive,
+  },
 }
 
 local M = {}
 
 local get_hl = vim.api.nvim_get_hl_by_name
 
-local format = function (arg)
+local format = function(arg)
   return "#" .. string.format("%06x", arg)
 end
 
 local giv_foreground = function(group)
-  return format(get_hl(group,true).foreground)
+  return format(get_hl(group, true).foreground)
 end
 
 local giv_background = function(group)
-  return format(get_hl(group,true).background)
+  return format(get_hl(group, true).background)
 end
 
 local used_theme = {
@@ -50,16 +50,16 @@ local used_theme = {
   blue = giv_foreground("Title"),
 }
 
-
-M.setup = function()
-	feline.setup({
-		components = components,
-		theme = used_theme,
-	})
+M.setup = function(opts)
+  used_theme = vim.tbl_deep_extend("force", {}, used_theme, opts or {})
+  feline.setup({
+    components = components,
+    theme = used_theme,
+  })
 end
 
 M.setup_winbar = function()
-	feline.winbar.setup({ components = components_winbar })
+  feline.winbar.setup({ components = components_winbar })
 end
 
 return M
