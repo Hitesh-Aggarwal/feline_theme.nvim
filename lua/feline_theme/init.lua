f8187981fdcb56827d4155417b370b1e5e7d6686
Hitesh Aggarwal <hitesh33231@gmail.com>
2023-01-28 12:31:06 +0530

    don't raise errors, instead use default colors

--- lua/feline_theme/init.lua
+++ lua/feline_theme/init.lua
@@ -24,30 +24,55 @@ local components_winbar = {
 
 local M = {}
 
-local get_hl = vim.api.nvim_get_hl_by_name
+local get_hl = function(group)
+  local status_ok, hl = pcall(vim.api.nvim_get_hl_by_name, group, true)
+  if status_ok then
+    return hl
+  else
+    return false
+  end
+end
 
 local format = function(arg)
   return "#" .. string.format("%06x", arg)
 end
 
 local giv_foreground = function(group)
-  return format(get_hl(group, true).foreground)
+  local col = get_hl(group)
+  if col then
+    if col.foreground == nil then
+      return false
+    else
+      return format(col.foreground)
+    end
+  else
+    return false
+  end
 end
 
 local giv_background = function(group)
-  return format(get_hl(group, true).background)
+  local col = get_hl(group)
+  if col then
+    if col.background == nil then
+      return false
+    else
+      return format(col.background)
+    end
+  else
+    return false
+  end
 end
 
 local used_theme = {
-  fg = giv_foreground("Normal"),
-  bg = giv_background("CursorLine"),
-  bg_dark = giv_background("Normal"),
-  green = giv_foreground("DiffAdded"),
-  yellow = giv_foreground("WarningMsg"),
-  purple = giv_foreground("Special"),
-  orange = giv_foreground("Number"),
-  red = giv_foreground("Error"),
-  blue = giv_foreground("Title"),
+  fg = giv_foreground("Normal") or "#ffffff",
+  bg = giv_background("CursorLine") or "#333333",
+  bg_dark = giv_background("Normal") or "#111111",
+  green = giv_foreground("DiffAdded") or "#00ff00",
+  yellow = giv_foreground("WarningMsg") or "#ffff00",
+  purple = giv_foreground("Special") or "#a020f0",
+  orange = giv_foreground("Number") or "#ffa500",
+  red = giv_foreground("Error") or "#ff0000",
+  blue = giv_foreground("Title") or "#0000ff",
 }
 
 M.setup = function(opts)
